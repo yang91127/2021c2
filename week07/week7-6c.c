d5b2c1c0a9bdd2200eb27a99d5b4d43651d77d90
yang91127 <10160916@ms1.mcu.edu.tw>
2022-04-13 21:37:43 +0800

    加入week07

---
+++ week07/week7-6c.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[2000][80];
+
+int compare( const void *pl, const void *p2 )
+{
+    strcmp( (char*)p1, (char*)p2 );
+}
+
+int main()
+{
+    int N;
+    scanf("%d\n", &N);
+
+    for(int i=0; i<N; i++){
+        scanf("%s", line[i] );
+        char others[80];
+        gets(  others  );
+    }
+
+    qsort( line, N, 80, compare );
+
+    for(int i=0; i<N; i++){
+        printf("%s\n", line[i] );
+    }
+
+}
